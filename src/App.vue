46b15c3209f9006dabf72a72ebba670771bc19a3
marcocampisi <marco.campisi@outlook.it>
2023-07-17 15:32:46 +0200

    Add vue components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+
     export default {
         data() {
             return {
 
             }
+        },
+        components: {
+            HeaderComponent,
+            MainComponent
         }
     }
 </script>
