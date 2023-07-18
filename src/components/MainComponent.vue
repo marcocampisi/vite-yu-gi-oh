a6500bd298786189d31e1c804047ce38863d7da7
marcocampisi <marco.campisi@outlook.it>
2023-07-18 16:11:26 +0200

    Add archetype filter

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -6,11 +6,40 @@ export default {
     name: 'MainComponent',
     data() {
         return {
-            store
+            store,
+            archetype: "",
+            archetypeOptions: []
         }
     },
     components: {
         CardComponent
+    },
+    computed: {
+        filteredCards() {
+            if (this.archetype === "") {
+                return this.store.cards;
+            } else {
+                return this.store.cards.filter(card => card.archetype === this.archetype);
+            }
+        },
+    },
+    methods: {
+        getArchetypeOptions() {
+            const archetypes = new Set();
+            for (const card of this.store.cards) {
+                archetypes.add(card.archetype);
+            }
+            this.archetypeOptions = Array.from(archetypes);
+        },
+    },
+    created() {
+        const archetypes = new Set();
+        for (const card of this.store.cards) {
+            if (card.archetype) {
+                archetypes.add(card.archetype);
+            }
+        }
+        this.archetypeOptions = Array.from(archetypes);
     }
 }
 </script>
@@ -19,21 +48,21 @@ export default {
     <div class="container-fluid">
         <div class="container">
             <div class="col-sm-2 my-5">
-                <select class="form-select" aria-label="Filter" v-on:change="getCards">
-                    <option value="1">Alien</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                <select class="form-select" aria-label="Filter" v-model="archetype">
+                    <option value="">Seleziona un archetipo</option>
+                    <option v-for="archetypeOption in archetypeOptions" :key="archetypeOption" :value="archetypeOption">
+                        {{ archetypeOption }}
+                    </option>
                 </select>
             </div>
         </div>
         <div class="container bg-white d-flex">
             <div class="row w-100 justify-content-between p-5">
-                <div class="col-md-3" v-for="(card, i) in store.cards" :key="card.id">
+                <div class="col-md-3" v-for="(card, i) in filteredCards" :key="card.id">
                     <CardComponent :store="card" />
                 </div>
             </div>
         </div>
-
     </div>
 </template>
 
